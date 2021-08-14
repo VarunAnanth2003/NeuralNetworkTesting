6c1f0f7ad4e0fc83d9775fbd6f6bdf292ef8f871
Varun Ananth <varunananth1@gmail.com>
2021-08-14 01:38:37 -0700

    next steps commented in main

--- Main.java
+++ Main.java
@@ -7,5 +7,9 @@ public class Main {
         Network n = new Network(100, 16, 16, 5);
         n.initializeNetwork(/*pixeldata should go here*/);
         double[] result = n.pulse();
+        //TODO: Get pixel data from 100px by 100px images
+        //TODO: Find a way to average cost over multiple training data images
+        //TODO: Use average cost to select best NNs
+        //TODO: Breed NN for performance with variable noise and see results
     }
 }
